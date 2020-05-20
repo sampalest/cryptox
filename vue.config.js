9d4e889e08b5aa4821166bdaa7da83ecb57fa786
Sampalest <16646954+sampalest@users.noreply.github.com>
2020-05-20 21:25:12 +0200

    config build for mac -> external file (bug fixed)

--- vue.config.js
+++ vue.config.js
@@ -14,45 +14,5 @@ module.exports = {
         jQuery: 'jquery',
       })
     ]
-  },
-  pluginOptions: {
-    electronBuilder: {
-      name: "Cryptox",
-      appId: "com.sampalest.cryptox",
-      buildVersion: "1",
-      afterSign: "scripts/notarize.js",
-      asar: false,
-      compression: "normal",
-      darkModeSupport: false,
-      type: "development",     
-      fileAssociations: {
-        ext: "ctx",
-        name: "CTX",
-        role: "Editor",
-        description: "Cryptox file encrypted"
-      },
-      mac: {
-        title: "Cryptox",
-        name: "Cryptox",
-        productName: "Cryptox",
-        executableName: "Cryptox",
-        category: "public.app-category.utilities",
-        hardenedRuntime: true,
-        gatekeeperAssess: false,
-        entitlements: "./entitlements.plist",
-        entitlementsInherit: "./entitlements.plist",
-        target: [
-          "zip",
-          "dmg"
-        ]
-      },
-      win: {
-        target: "msi"
-      },
-      linux: {
-        target: ["AppImage", "deb", "tar.gz"]
-      },
-      nodeModulesPath: ['./node_modules']
-    },
   }
 }
