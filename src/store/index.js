0b21c5ba055d763ac2816829e1a1e1b2d7794ee8
Sam <sam@MacBook-Pro-de-Sam.local>
2020-05-24 14:19:29 +0200

    OpenFile => open file events with file associations.

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,34 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import { createPersistedState, createSharedMutations } from "vuex-electron";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     state: {
+        files: null
     },
     mutations: {
+        setFiles(state, files) {
+			state.files = files;
+        },
+        deleteFiles(state) {
+            state.files = null;
+        }
     },
     actions: {
+        setFiles: function ({ commit }, files) {
+            commit("setFiles", files);
+        },
+        delFiles: function({ commit }) {
+            commit("deleteFiles");
+        }
     },
-    modules: {
-    }
+    getters: {
+		files: state => state.files
+    },
+    plugins: [
+        createPersistedState(),
+        createSharedMutations()
+    ],
 });
